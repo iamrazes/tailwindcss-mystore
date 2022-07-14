ec88b19b673833d6c8d76b77971c6d86d6448c18
Syahrul Safarila <57931052+SySafarila@users.noreply.github.com>
2022-07-14 15:24:19 +0700

    top & bottom navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        base: "#212121",
+        "base-light": "#323232",
+        "base-dark": "#0A0A0A",
+      },
+    },
   },
   plugins: [],
-}
+};
