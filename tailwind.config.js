7d0287212b8ce553dfc466ab36c8362cd0e3c3e5
Syahrul Safarila <57931052+SySafarila@users.noreply.github.com>
2022-07-17 21:33:30 +0700

    order detail

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
       colors: {
         base: "#212121",
         "base-light": "#323232",
+        "base-light-2": "#575757",
         "base-dark": "#0A0A0A",
         accent: {
           yellow: "#FFB800",
